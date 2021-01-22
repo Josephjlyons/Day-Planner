654cb5b42812bc03f81b4f57ea60bc6b555a3936
Joe Lyons <josephjlyons90@gmail.com>
2021-01-21 20:38:22 -0500

    data id add... local storage in progress

--- planner.js
+++ planner.js
@@ -1,10 +1,10 @@
-let plannerText = "";
-let appointmenttime = "";
+let appointmentTime = "";
+let appointmentDate = "";
 let currentDate = document.getElementById("currentDate");
 let currentTime = document.getElementById("currentTime");
 let storedAppoint;
 let returnedAppoint;
-let hourContainer;
+let appointArr = [];
 let currentContainer;
 const container = document.getElementsByClassName("container")
 
@@ -16,27 +16,41 @@ function showDateTime() {
 }; showDateTime()
 
 
-
 // -------------------------------- //
 // Have to make certain containers colors depending on time (PAST-PRESENT-FUTURE)
 pastPresFuture();
-function pastPresFuture(){
+function pastPresFuture() {
     for (i = 0; i <= 23; i++) {
         currentContainer = i;
         currentTime = dayjs().format("H")
-
-    if (currentTime > i) {
-        $("#" + currentContainer).addClass("past");
-        $("#" + currentContainer).children("div").children("appointmentNotes").addClass("past");
+        
+        if (currentTime > i) {
+            $("#" + currentContainer).addClass("past");
+            $("#" + currentContainer).children("div").children("appointmentNotes").addClass("past");
+        }
+        else if (currentTime == i) {
+            $("#" + currentContainer).addClass("present");
+            $("#" + currentContainer).children("div").children("appointmentNotes").addClass("present");
+        }
+        else if (currentTime < i) {
+            $("#" + currentContainer).addClass("future");
+            $("#" + currentContainer).children("div").children("appointmentNotes").addClass("future");
+        }
+        
     }
-    else if (currentTime == i) {
-        $("#" + currentContainer).addClass("present");
-        $("#" + currentContainer).children("div").children("appointmentNotes").addClass("present");
-    }
-    else if (currentTime < i) {
-        $("#" + currentContainer).addClass("future");
-        $("#" + currentContainer).childer("div").children("appointmentNotes").addClass("future");
-    }
+}
 
-}}
 
+
+
+// Saving notes to local enabling save button
+$(".saveBtn").on("click", function(){
+    let hour = $(this).attr("data-hour");
+    console.log(hour);
+    let textarea = $("#" + hour);
+    console.log(textarea.val());
+
+
+localStorage.setItem(hour, JSON.stringify(textarea)
+)});
+// populat appointment in appropiate area
